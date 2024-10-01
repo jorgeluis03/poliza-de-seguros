4017732d1e12703b9e344f998d8b925556709b69
xt8736 <xt8736@rimac.com.pe>
2024-10-01 17:44:47 -0500

    documentacion con api swagger personalizada

---
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("APIs de Gestion de Polzias de Seguros")
+						.version("1.0")
+						.description("Estas son las APIs usadas en el backend de Gestion de Polizas de Seguros")
+						.license(new License()
+									.name("Licencia Apache 2.0")
+									.url("http://springdoc.org")));
+	}
+	
+}
